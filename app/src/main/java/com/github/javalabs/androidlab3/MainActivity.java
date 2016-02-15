4a5e6b4409e96c13e3ccaa11935b5ae3ed78c54e
Yegorov <yegorov0725@yandex.ru>
2016-02-15 18:55:29 +0300

    Some fix

--- app/src/main/java/com/github/javalabs/androidlab3/MainActivity.java
+++ app/src/main/java/com/github/javalabs/androidlab3/MainActivity.java
@@ -16,7 +16,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int notifyID = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,26 +27,26 @@ public class MainActivity extends AppCompatActivity {
         switch (v.getId()) {
             case R.id.btn_alert: {
                 AlertDialog.Builder b = new AlertDialog.Builder(this);
-                b.setTitle("Заголовок");
+                //b.setTitle("Заголовок");
                 View view = (RelativeLayout) getLayoutInflater().inflate(R.layout.alert_layout, null);
                 b.setView(view);
-                ((TextView) view.findViewById(R.id.textView)).setText("Тут текст сообщения");
+                ((TextView) view.findViewById(R.id.textView)).setText("Тут текст сообщения (AlertDialog)");
                 b.show();
+                this.notifyID = 1;
                 break;
             }
             case R.id.btn_notification: {
                 Context context = getApplicationContext();
                 NotificationCompat.Builder b = new NotificationCompat.Builder(context);
                 RemoteViews r = new RemoteViews(this.getPackageName(), R.layout.alert_layout);
-                r.setTextViewText(R.id.textView, "Проверка уведомления");
+                r.setTextViewText(R.id.textView, "Проверка уведомления (Notification) id=" + String.valueOf(notifyID));
                 b.setContent(r);
                 b.setSmallIcon(R.mipmap.ic_launcher); // 02-14 19:51:14.802 400-1703/system_process E/NotificationService: Not posting notification with icon==0: Notification(pri=0 contentView=com.github.javalabs.androidlab3/0x7f04001a vibrate=null sound=null defaults=0x0 flags=0x0 kind=[null])
-
+                b.setTicker("Текст, который появится в строке состояния");
                 NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-                nm.notify(42, b.build());
-                Toast.makeText(this, "Notify2", Toast.LENGTH_SHORT).show();
+                nm.notify(notifyID, b.build());
 
-                //Notify();
+                notifyID++;
                 break;
             }
         }
@@ -54,19 +54,15 @@ public class MainActivity extends AppCompatActivity {
     private void Notify(){
         NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext());
 
-        builder
-                .setSmallIcon(R.mipmap.image)
-                // большая картинка
+        builder.setSmallIcon(R.mipmap.image)
                 //.setTicker(res.getString(R.string.warning)) // текст в строке состояния
-                .setTicker("Последнее китайское предупреждение!")
+                .setTicker("Текст, который появится в строке!")
                 .setWhen(System.currentTimeMillis())
                 .setAutoCancel(true)
                 //.setContentTitle(res.getString(R.string.notifytitle)) // Заголовок уведомления
                 .setContentTitle("Напоминание")
                 //.setContentText(res.getString(R.string.notifytext))
-                .setContentText("Пора покормить кота"); // Текст уведомления
-
-        // Notification notification = builder.getNotification(); // до API 16
+                .setContentText("Уведомление"); // Текст уведомления
         Notification notification = builder.build();
 
         NotificationManager notificationManager = (NotificationManager) getApplicationContext()
